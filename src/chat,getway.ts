9fdc8dddcb2dd70e53484ea2406c81a7f85caac3
jokerf6 <fhakem75@gmail.com>
2023-04-05 09:03:59 +0200

    port3

--- src/chat,getway.ts
+++ src/chat,getway.ts
@@ -14,7 +14,7 @@ import { Logger } from '@nestjs/common';
 import { Socket, Server } from 'socket.io';
 import { messageType } from '@prisma/client';
 
-@WebSocketGateway(80, { cors: '*' })
+@WebSocketGateway()
 export class chatGetway implements OnGatewayConnection, OnGatewayInit {
   constructor(
     private readonly prisma: PrismaService,
